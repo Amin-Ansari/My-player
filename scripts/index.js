7b9474faa9df8ad4b4677c48150acb8206fade30
Amin Ansari <devansari@yahoo.com>
2022-09-17 12:01:25 +0430

    Mobile version got fixed

--- scripts/index.js
+++ scripts/index.js
@@ -2,15 +2,17 @@ function ScrollBase(theElement) {
   let theOpacity = 1;
   this.theElement = theElement;
   this.calculateTransform = function () {
-    let theBoundingTop = theElement.getBoundingClientRect();
-    theBoundingTop = theBoundingTop.top / 10 - 16;
-    if (theBoundingTop > 0 && theBoundingTop < 58) {
-      this.theElement.style = `transform: rotateX(-${theBoundingTop}deg);`;
-    }
-    if (theBoundingTop > 0) {
-      this.theElement.style.opacity = `${theOpacity / theBoundingTop + 0.3}`;
-    } else {
-      this.theElement.style.opacity = `${theOpacity}`;
+    if (window.innerWidth >= 576) {
+      let theBoundingTop = theElement.getBoundingClientRect();
+      theBoundingTop = theBoundingTop.top / 10 - 16;
+      if (theBoundingTop > 0 && theBoundingTop < 58) {
+        this.theElement.style = `transform: rotateX(-${theBoundingTop}deg);`;
+      }
+      if (theBoundingTop > 0) {
+        this.theElement.style.opacity = `${theOpacity / theBoundingTop + 0.3}`;
+      } else {
+        this.theElement.style.opacity = `${theOpacity}`;
+      }
     }
   };
 }
